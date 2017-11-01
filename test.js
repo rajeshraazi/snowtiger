48f2bccf2991d8c45f0860f1ad37954f9d04ea7f
Rajesh Kumar <rajesh.kumar@timesinternet.in>
2017-11-01 14:59:49 +0530

    add file to root

--- test.js
+++ test.js
@@ -1,1 +1,4 @@
-// This file needs to be setup at root for all configuration and settings.
+// This file needs to be setup at root for all configuration and settings
+
+// This is sample file is created to manage all the dependecies.
+
